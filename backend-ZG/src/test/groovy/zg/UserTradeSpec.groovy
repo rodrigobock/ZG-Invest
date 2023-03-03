121f3aed492a3f7df6cdf88e4d367a54c9162800
Rodrigo Cabral Bock <rodrigocabralbock@gmail.com>
2023-03-03 00:22:11 -0300

    Create UserTradeSpec.groovy

---
+++ backend-ZG/src/test/groovy/zg/UserTradeSpec.groovy
@@ -0,0 +1,19 @@
+package zg
+
+
+import grails.testing.gorm.DomainUnitTest
+import spock.lang.Specification
+
+class UserTradeSpec extends Specification implements DomainUnitTest<UserTrade> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
