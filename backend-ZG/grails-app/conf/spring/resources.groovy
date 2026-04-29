45c4b64b06587cef0b26bed3da97bdc355a75731
Rodrigo Cabral Bock <rodrigocabralbock@gmail.com>
2026-04-29 00:37:46 -0300

    feat: override OpenSessionInView interceptors with NoOp implementation to disable lazy loading behavior

--- backend-ZG/grails-app/conf/spring/resources.groovy
+++ backend-ZG/grails-app/conf/spring/resources.groovy
@@ -1,3 +1,5 @@
 // Place your Spring DSL code here
 beans = {
+    openSessionInViewInterceptor(zg.NoOpInterceptor)
+    osivInterceptor(zg.NoOpInterceptor)
 }
---
+++ backend-ZG/src/main/groovy/zg/NoOpInterceptor.groovy
@@ -0,0 +1,6 @@
+package zg
+
+import org.springframework.web.servlet.HandlerInterceptor
+
+class NoOpInterceptor implements HandlerInterceptor {
+}
